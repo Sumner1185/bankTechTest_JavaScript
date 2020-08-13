4981ba29eea13b7e188a778661eab9c4fde78e03
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 13:13:02 +0100

    Test for starting balance of 0 green

--- src/__test__/account.spec.js
+++ src/__test__/account.spec.js
@@ -4,6 +4,7 @@ describe('Account', function () {
   let account;
 
   test('Should start with an initial balance of 0', function () {
+    let account = new Account();
     expect(account.startingBalance).toEqual(0)
   });
 });
