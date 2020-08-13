4229de28b65355200f81bae529d339dc748b34d3
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 17:00:43 +0100

    Add test for showStatement function to display required formatted line

--- src/__test__/transaction.spec.js
+++ src/__test__/transaction.spec.js
@@ -1,11 +1,12 @@
-const sinon = require('sinon');
+const sinon = require('sinon')
+const MockDate = require('mockdate')
 const Transaction = require('../transaction.js');
 
 describe('Transaction', () => {
   let transaction;
 
   beforeEach(() => {
-    transaction = new Transaction({credit: 1000, debit: 500, balance: 1500})
+    transaction = new Transaction({ credit: 1000, debit: 500, balance: 1500 })
   })
 
   test('Should initialise with a credit amount if applicable', () => {
@@ -19,4 +20,18 @@ describe('Transaction', () => {
   test('Should initialise with a balance amount if applicable', () => {
     expect(transaction.balance).toEqual(1500);
   });
+
+    describe('showStatement', () => {
+  
+      test('Should have the date displayed correctly as the first element', () => {
+        const mockedDate = new Date(2020, 7, 12);
+        const realDateNow = Date.now.bind(global.Date);
+        global.Date = jest.fn(() => mockedDate)
+        let transaction = new Transaction({ credit: 2000, debit: 1000, balance: 1500 })
+
+        expect(transaction.showStatement()).toEqual("12/08/2020 || 2000.00 || 1000.00 || 1500.00 ");
+
+        global.Date.now = realDateNow;
+      });
+    });
 });
--- src/transaction.js
+++ src/transaction.js
@@ -1,9 +1,27 @@
+const moment = require('moment')
+
 class Transaction {
   constructor ({ credit = null, debit = null, balance = null}) {
     this.credit = credit,
     this.debit = debit,
     this.balance = balance
+    this.date = new Date()
+  };
+
+  showStatement() {
+    return [
+      this._formatDate(),
+      this._formatCurrency(this.credit),
+      this._formatCurrency(this.debit),
+      this._formatCurrency(this.balance)
+    ].join("|| ");
   }
-}
+
+  _formatDate = () => moment(this.date).format("DD/MM/YYYY ");
+
+  _formatCurrency = (item) => item != null ? `${this._formatDecimals(item)} ` : ""
+
+  _formatDecimals = (value) => parseFloat(value).toFixed(2)
+};
 
 module.exports = Transaction;
