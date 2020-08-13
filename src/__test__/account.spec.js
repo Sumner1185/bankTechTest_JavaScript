773e4f7ee881d7ff0ea2ada9415f146df850ea1b
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 13:26:12 +0100

    Refactor test file with beforeEach for accoun object

--- src/__test__/account.spec.js
+++ src/__test__/account.spec.js
@@ -3,21 +3,21 @@ const Account = require('../account.js')
 describe('Account', function () {
   let account;
 
-  test('Should start with an initial balance of 0', function () {
-    let account = new Account();
+  beforeEach(() => {
+    account = new Account()
+  })
 
+  test('Should start with an initial balance of 0', function () {
     expect(account.balance).toEqual(0)
   });
 
   test('Should allow a user to add money to their account', function () {
-    let account = new Account();
     account.add(1000);
 
     expect(account.balance).toEqual(1000)
   });
 
   test('Should allow a user to withdraw money from their account', function () {
-    let account = new Account();
     account.add(1000);
     account.withdraw(500);
 
