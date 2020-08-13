f5ac5ae0a28beba028111171322fec084de87d12
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 13:17:12 +0100

    Second failing test for add function

--- src/__test__/account.spec.js
+++ src/__test__/account.spec.js
@@ -5,7 +5,14 @@ describe('Account', function () {
 
   test('Should start with an initial balance of 0', function () {
     let account = new Account();
-    
+
     expect(account.balance).toEqual(0)
+  });
+
+  test('Should allow a user to add money to their account', function () {
+    let account = new Account();
+    account.add(1000)
+
+    expect(account.balance).toEqual(1000)
   });
 });
