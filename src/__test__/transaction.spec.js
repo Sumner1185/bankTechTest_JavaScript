3f6d5dad6143eebf7b36e1574778f9cf1dccac66
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 15:37:35 +0100

    Refactor test file using beforeEach for transaction object

--- src/__test__/transaction.spec.js
+++ src/__test__/transaction.spec.js
@@ -2,22 +2,21 @@ const sinon = require('sinon');
 const Transaction = require('../transaction.js');
 
 describe('Transaction', () => {
+  let transaction;
+
+  beforeEach(() => {
+    transaction = new Transaction({credit: 1000, debit: 500, balance: 1500})
+  })
 
   test('Should initialise with a credit amount if applicable', () => {
-    let transaction = new Transaction({ credit: 1000 });
-
     expect(transaction.credit).toEqual(1000);
   });
 
   test('Should initialise with a debit amount if applicable', () => {
-    let transaction = new Transaction({ debit: 500 });
-
     expect(transaction.debit).toEqual(500);
   });
 
   test('Should initialise with a balance amount if applicable', () => {
-    let transaction = new Transaction({ balance: 1500 });
-
     expect(transaction.balance).toEqual(1500);
   });
 });
