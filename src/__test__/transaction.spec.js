00a5af182d449611c5fbce347e7b47c0f49c4ae1
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 17:09:56 +0100

    Add tests for no input value for credit, debit and balance

--- src/__test__/transaction.spec.js
+++ src/__test__/transaction.spec.js
@@ -22,16 +22,30 @@ describe('Transaction', () => {
   });
 
     describe('showStatement', () => {
-  
-      test('Should have the date displayed correctly as the first element', () => {
+      beforeAll(() => {
         const mockedDate = new Date(2020, 7, 12);
         const realDateNow = Date.now.bind(global.Date);
         global.Date = jest.fn(() => mockedDate)
+      })
+
+      afterAll(() => {
+        global.Date.now = realDateNow;
+      });
+  
+      test('Should have the date displayed correctly as the first element', () => {
         let transaction = new Transaction({ credit: 2000, debit: 1000, balance: 1500 })
 
         expect(transaction.showStatement()).toEqual("12/08/2020 || 2000.00 || 1000.00 || 1500.00 ");
+      });
 
-        global.Date.now = realDateNow;
+      test('Should pass in an empty string if no value given', () => {
+        let transaction1 = new Transaction({ credit: 2000 })
+        let transaction2 = new Transaction({ debit: 1000 })
+        let transaction3 = new Transaction({ balance: 1500 })
+
+        expect(transaction1.showStatement()).toEqual("12/08/2020 || 2000.00 || || ");
+        expect(transaction2.showStatement()).toEqual("12/08/2020 || || 1000.00 || ");
+        expect(transaction3.showStatement()).toEqual("12/08/2020 || || || 1500.00 ");
       });
     });
 });
