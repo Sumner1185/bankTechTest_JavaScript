61692f18e80d04573c45d02bf1d46dc21820328d
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-13 13:20:16 +0100

    Add updated package.json file

---
+++ src/account.js
@@ -0,0 +1,13 @@
+class Account {
+  constructor() {
+    const STARTING_BALANCE = 0;
+
+    this.balance = STARTING_BALANCE
+  }
+
+  add = function (amount) {
+    this.balance += amount
+  }
+};
+
+module.exports = Account
