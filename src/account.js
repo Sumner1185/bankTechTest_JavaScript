0cd4fce4f74ea6441cc1e097d232dcd34f8d347f
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-14 11:09:11 +0100

    Refactor removing addTransaction logic out to own function

--- src/account.js
+++ src/account.js
@@ -15,15 +15,19 @@ class Account {
 
   add (amount) {
     this.balance += amount
-    this.transactionLog.unshift(new this.transactionClass({ credit: amount, balance: this.balance }))
+    this.addTransaction({ credit: amount, balance: this.balance });
     return `${this.formatClass.currency(amount)}deposited. Current balance: ${this.formatClass.currency(this.balance)}`
   }
 
   withdraw (amount) {
     if (amount > this.balance) return "Insufficient funds";
     this.balance -= amount
-    this.transactionLog.unshift(new this.transactionClass({ debit: amount, balance: this.balance }))
+    this.addTransaction({ debit: amount, balance: this.balance });
     return `${this.formatClass.currency(amount)}withdrawn. Current balance: ${this.formatClass.currency(this.balance)}`
+  }
+
+  addTransaction (transObj) {
+    this.transactionLog.unshift(new this.transactionClass(transObj))
   }
 };
 
